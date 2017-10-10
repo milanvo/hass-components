fddf666729b17c0c6296dcf6ae7ed2712b812aea
milanvo <milanvo@users.noreply.github.com>
2017-10-10 12:08:32 +0000

    Code cleanup and lint

--- ups_pico.py
+++ ups_pico.py
@@ -9,7 +9,8 @@ import logging
 
 # import voluptuous as vol
 
-from homeassistant.helpers.entity import Entity, ToggleEntity
+from homeassistant.components.switch import SwitchDevice
+from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_component import EntityComponent
 from homeassistant.helpers.event import async_track_time_interval
 
@@ -19,6 +20,7 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'ups_pico'
 ENTITY_ID_FORMAT = DOMAIN + '.{}'
+SWITCH_ENTITY_ID_FORMAT = 'switch.' + DOMAIN + '_{}'
 
 SENSOR_TYPES = {
     'voltBat': ['BAT Voltage', 'V', 'battery'],
@@ -40,7 +42,7 @@ def async_setup(hass, config):
     component = EntityComponent(_LOGGER, DOMAIN, hass)
     entities = []
     ups_pico = UpsPico()
-    ups_pico.getData()
+    ups_pico.get_data()
 
     for object_id, cfg in SENSOR_TYPES.items():
         name = cfg[0]
@@ -58,17 +60,18 @@ def async_setup(hass, config):
     if not entities:
         return False
 
-    async_track_time_interval(hass, ups_pico.getData, component.scan_interval)
+    async_track_time_interval(hass, ups_pico.async_update,
+                              component.scan_interval)
 
     yield from component.async_add_entities(entities)
     return True
 
 
 class UpsPicoSensor(Entity):
-    """Representation of UPS PIco sensor"""
+    """Representation of UPS PIco sensor."""
 
     def __init__(self, ups_pico, object_id, name, unit, icon):
-        """Initialize the sensor"""
+        """Initialize the sensor."""
         self.ups_pico = ups_pico
         self.entity_id = ENTITY_ID_FORMAT.format(object_id)
         self._object_id = object_id
@@ -102,17 +105,31 @@ class UpsPicoSensor(Entity):
         """No polling needed."""
         return True
 
+    @property
+    def state_attributes(self):
+        """Return the state attributes of the UPS."""
+        if self._object_id == 'pwrMode':
+            attrs = {
+                'pwrRunTime': self.ups_pico.pico_data['pwrRunTime'],
+                'verPCB': self.ups_pico.pico_data['verPCB'],
+                'verBoot': self.ups_pico.pico_data['verBoot'],
+                'verFW': self.ups_pico.pico_data['verFW'],
+            }
+            return attrs
+        return None
+
     def update(self):
         """Update sensor state."""
-        self._state = self.ups_pico.picoData[self._object_id]
+        self._state = self.ups_pico.pico_data[self._object_id]
 
-class UpsPicoSwitch(ToggleEntity):
-    """Representation of UPS PIco switch"""
+
+class UpsPicoSwitch(SwitchDevice):
+    """Representation of UPS PIco switch."""
 
     def __init__(self, ups_pico, object_id, name, icon):
         """Initialize the switch."""
         self.ups_pico = ups_pico
-        self.entity_id = ENTITY_ID_FORMAT.format(object_id)
+        self.entity_id = SWITCH_ENTITY_ID_FORMAT.format(object_id)
         self._object_id = object_id
         self._name = name
         self._state = None
@@ -140,30 +157,32 @@ class UpsPicoSwitch(ToggleEntity):
 
     def turn_on(self, **kwargs):
         """Turn the device on."""
-        self.ups_pico.ledOn(self._object_id)
+        self.ups_pico.led_on(self._object_id)
         self._state = True
         self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs):
         """Turn the device off."""
-        self.ups_pico.ledOff(self._object_id)
+        self.ups_pico.led_off(self._object_id)
         self._state = False
         self.schedule_update_ha_state()
 
     def update(self):
         """Update switch state."""
-        self._state = self.ups_pico.picoData[self._object_id]
+        self._state = self.ups_pico.pico_data[self._object_id]
+
 
 class UpsPico(object):
-    """Class for UPS PIco i2c interface"""
+    """Class for UPS PIco i2c interface."""
 
     def __init__(self):
+        """Initialize class."""
         import smbus2
 
-        self.picoReg = dict()
-        self.picoData = dict()
+        self.pico_reg = dict()
+        self.pico_data = dict()
         self.i2c = smbus2.SMBus(1)
-        self.regDict = {
+        self.reg_dict = {
             "ledOrange": 0x09,
             "ledGreen": 0x0a,
             "ledBlue": 0x0b,
@@ -171,316 +190,117 @@ class UpsPico(object):
         }
         return
 
-    def _tryGetData(self):
+    @asyncio.coroutine
+    def async_update(self, *_):
+        """Async update latest data."""
+        self.get_data()
+
+    def _try_get_data(self):
         try:
             reg = self.i2c.read_i2c_block_data(0x69, 0, 0x1d)
             reg += [0xff, 0xff, 0xff]
             reg += (self.i2c.read_i2c_block_data(0x69, 0x20, 7))
-            self.picoReg[0x69] = reg
+            self.pico_reg[0x69] = reg
 
             reg = self.i2c.read_i2c_block_data(0x6b, 0, 0x16)
-            self.picoReg[0x6b] = reg
+            self.pico_reg[0x6b] = reg
 
-        except Exception as e:
-            _LOGGER.error('Except class PicoPlugin _tryGetData(): ' + str(e))
+        except Exception as exc:
+            _LOGGER.error('Except class UPS PIco _try_get_data(): ' + str(exc))
             return False
 
         return True
 
-    def setData(self, device, data):
+    def set_data(self, device, data):
+        """Set data to UPS PIco."""
         addr = 0x6b
-        if device in self.regDict:
-            reg = self.regDict[device]
+        if device in self.reg_dict:
+            reg = self.reg_dict[device]
             try:
                 self.i2c.write_byte_data(addr, reg, data)
                 return True
-            except Exception as e:
-                _LOGGER.error('Except class PicoPlugin setData(): ' + str(e))
+            except Exception as exc:
+                _LOGGER.error('Except class PicoPlugin setData(): %s',
+                              str(exc))
         else:
-            _LOGGER.error('Error class PicoPlugin setData(): Unknown device',
-                          device)
+            _LOGGER.error('Error class PicoPlugin setData():'
+                          'Unknown device %s', device)
 
         return False
 
-    def ledOn(self, device):
-        return self.setData(device, 1)
+    def led_on(self, device):
+        """Turn LED on."""
+        return self.set_data(device, 1)
 
-    def ledOff(self, device):
-        return self.setData(device, 0)
+    def led_off(self, device):
+        """Turn LED off."""
+        return self.set_data(device, 0)
 
-    def getData(self):
-        result = self._tryGetData()
+    def get_data(self):
+        """Get data from UPS PIco."""
+        result = self._try_get_data()
 
         if not result:
             return False
 
         # *** 0x69 registers
-        data = self.picoReg[0x69]
+        data = self.pico_reg[0x69]
 
         # 0x69 0x00 Powering mode
         reg_word = data[0x00]
         if reg_word == 1:
-            self.picoData["pwrMode"] = "RPi powered"
+            self.pico_data["pwrMode"] = "RPi powered"
         elif reg_word == 2:
-            self.picoData["pwrMode"] = "UPS powered"
+            self.pico_data["pwrMode"] = "UPS powered"
 
         # 0x69 0x08 BAT voltage
         reg_word = int.from_bytes(data[0x08:0x0a], byteorder="little")
         reg_hex = format(reg_word, "02x")
         reg_volt = float(reg_hex) / 100
-        self.picoData["voltBat"] = reg_volt
+        self.pico_data["voltBat"] = reg_volt
 
         # 0x69 0x0a RPi voltage
         reg_word = int.from_bytes(data[0x0a:0x0c], byteorder="little")
         reg_hex = format(reg_word, "02x")
         reg_volt = float(reg_hex) / 100
-        self.picoData["voltRpi"] = reg_volt
+        self.pico_data["voltRpi"] = reg_volt
 
         # 0x69 0x1b NTC1 temperature
         reg_word = data[0x1b]
         reg_hex = format(reg_word, "02x")
-        self.picoData["tempNtc1"] = reg_hex
+        self.pico_data["tempNtc1"] = reg_hex
 
         # 0x69 0x24 PCB version
         reg_word = data[0x24]
         reg_chr = chr(reg_word)
-        self.picoData["verPCB"] = reg_chr
+        self.pico_data["verPCB"] = reg_chr
 
         # 0x69 0x25 Bootloader version
         reg_word = data[0x25]
         reg_chr = chr(reg_word)
-        self.picoData["verBoot"] = reg_chr
+        self.pico_data["verBoot"] = reg_chr
 
         # 0x69 0x26 FW version
         reg_word = data[0x26]
         reg_hex = format(reg_word, "02x")
-        self.picoData["verFW"] = reg_hex
+        self.pico_data["verFW"] = reg_hex
 
         # *** 0x6b registers
-        data = self.picoReg[0x6b]
+        data = self.pico_reg[0x6b]
 
         # 0x6b 0x01 Bat Powering time
         reg_word = data[0x01]
         if reg_word == 0xff:
-            self.picoData["pwrRunTime"] = "disabled"
+            self.pico_data["pwrRunTime"] = "disabled"
         else:
-            self.picoData["pwrRunTime"] = 1 + reg_word
+            self.pico_data["pwrRunTime"] = 1 + reg_word
 
         # 0x6b 0x09, 0x0a, 0x0b User LEDs Orange, Green, Blue
-        self.picoData["ledOrange"] = data[0x09]
-        self.picoData["ledGreen"] = data[0x0a]
-        self.picoData["ledBlue"] = data[0x0b]
+        self.pico_data["ledOrange"] = data[0x09]
+        self.pico_data["ledGreen"] = data[0x0a]
+        self.pico_data["ledBlue"] = data[0x0b]
 
         # 0x6b 0x15 Enable LEDs
-        self.picoData["ledEnable"] = data[0x15]
+        self.pico_data["ledEnable"] = data[0x15]
 
         return True
-
-    def getTest(self):
-        """Testing code"""
-
-        reg1 = self.i2c.read_i2c_block_data(0x69, 0, 0x1d)
-        print("reg 0x69 0-0x1c:", reg1, "\nlength:", len(reg1))
-        reg2 = self.i2c.read_i2c_block_data(0x69, 0x20, 7)
-        print("reg 0x69 0x20-0x26::", reg2, "\nlength:", len(reg2))
-
-        data = self.i2c.read_word_data(0x69, 0x08)
-        print("\n")
-        print("BAT Voltage")
-        print("word 0x69 0x08:", data)
-        print("word 0x69 0x08 Format:", format(data, "02x"))
-        print("Volts:", str(float(format(data, "02x")) / 100))
-
-        data = self.i2c.read_word_data(0x69, 0x0a)
-        print("\n")
-        print("RPi Voltage")
-        print("word 0x69 0x0a:", data)
-        print("word 0x69 0x0a Format:", format(data, "02x"))
-        print("Volts:", str(float(format(data, "02x")) / 100))
-
-        result = self.getData()
-
-        if not result:
-            print("Failed getData")
-            return
-
-        print()
-        print("picoReg 0x69 0-0x26:", self.picoReg[0x69], "\nlength:", len(self.picoReg[0x69]))
-        print("picoReg 0x6b 0-0x15:", self.picoReg[0x6b], "\nlength:", len(self.picoReg[0x6b]))
-        print()
-        #print("BAT Voltage:", str(int.from_bytes(data[8:10], byteorder="little")), str(float(format(int.from_bytes(data[8:10], byteorder="little"), "02x"))/100))
- 
-        print("BAT Voltage (V): " + str(self.picoData["voltBat"]))
-        print("RPi Voltage (V): " + str(self.picoData["voltRpi"]))
-        print("NTC1 Temperature (deg. C): " + str(self.picoData["tempNtc1"]))
-        print("Powering mode: " + self.picoData["pwrMode"])
-        print("Bat Powering running time (min): " + str(self.picoData["pwrRunTime"]))
-        print("User LED Orange: " + str(self.picoData["ledOrange"]))
-        print("User LED Green: " + str(self.picoData["ledGreen"]))
-        print("User LED Blue: " + str(self.picoData["ledBlue"]))
-        print("Enable LEDs: " + str(self.picoData["ledEnable"]))
-        print("-"*20)
-        print("PCB version: " + self.picoData["verPCB"])
-        print("Bootloader version: " + self.picoData["verBoot"])
-        print("FW version: " + str(self.picoData["verFW"]))
-
-
-deviceDict = {
-        1: {
-            "Name": "BAT Voltage",
-            "Unit": 1,
-            "TypeName": "Voltage",
-            "picoName": "voltBat",
-        },
-        2: {
-            "Name": "RPi Voltage",
-            "Unit": 2,
-            "TypeName": "Voltage",
-            "picoName": "voltRpi",
-        },
-        3: {
-            "Name": "NTC1 Temperature",
-            "Unit": 3,
-            "TypeName": "Temperature",
-            "picoName": "tempNtc1",
-        },
-        4: {
-            "Name": "Powering Mode",
-            "Unit": 4,
-            "TypeName": "Text",
-            "picoName": "pwrMode",
-        },
-        5: {
-            "Name": "Orange LED",
-            "Unit": 5,
-            "TypeName": "Switch",
-            "picoName": "ledOrange",
-        },
-        6: {
-            "Name": "Green LED",
-            "Unit": 6,
-            "TypeName": "Switch",
-            "picoName": "ledGreen",
-        },
-        7: {
-            "Name": "Blue LED",
-            "Unit": 7,
-            "TypeName": "Switch",
-            "picoName": "ledBlue",
-        },
-        8: {
-            "Name": "Enabled LEDs",
-            "Unit": 8,
-            "TypeName": "Switch",
-            "picoName": "ledEnable",
-        },
-        99: {
-            "Name": "Test",
-            "Unit": 99,
-            "TypeName": "Switch",
-            "picoName": "Test",
-        },
-}
-
-"""
-    result = _pico.getData()
-    #Domoticz.Log("reg 0x69 0-0x26:" + str(data) + "\nlength:" + str(len(data)))
-    Domoticz.Log("PCB version: " + _pico.picoData["verPCB"])
-    Domoticz.Log("Bootloader version: " + _pico.picoData["verBoot"])
-    Domoticz.Log("FW version: " + str(_pico.picoData["verFW"]))
-    Domoticz.Log("Bat Powering running time (min): " + str(_pico.picoData["pwrRunTime"]))
-    Domoticz.Debug("-" * 20)
-    Domoticz.Debug("BAT Voltage: " + str(_pico.picoData["voltBat"]))
-    Domoticz.Debug("RPi Voltage: " + str(_pico.picoData["voltRpi"]))
-    Domoticz.Debug("NTC1 Temperature: " + str(_pico.picoData["tempNtc1"]))
-    Domoticz.Debug("Powering Mode: " + _pico.picoData["pwrMode"])
-    Domoticz.Debug("User LED Orange: " + str(_pico.picoData["ledOrange"]))
-    Domoticz.Debug("User LED Green: " + str(_pico.picoData["ledGreen"]))
-    Domoticz.Debug("User LED Blue: " + str(_pico.picoData["ledBlue"]))
-    Domoticz.Debug("Enable LEDs: " + str(_pico.picoData["ledEnable"]))
-
-    if result:
-        UpdateDevice(1, 0, str(_pico.picoData["voltBat"]), updEvery=True)
-        UpdateDevice(2, 0, str(_pico.picoData["voltRpi"]), updEvery=True)
-        UpdateDevice(3, 0, str(_pico.picoData["tempNtc1"]), updEvery=True)
-        UpdateDevice(4, 0, _pico.picoData["pwrMode"])
-        UpdateDevice(5, _pico.picoData["ledOrange"], "0")
-        UpdateDevice(6, _pico.picoData["ledGreen"], "0")
-        UpdateDevice(7, _pico.picoData["ledBlue"], "0")
-        UpdateDevice(8, _pico.picoData["ledEnable"], "0")
-
-    #DumpConfigToLog()
-
-def onStop():
-    Domoticz.Log("Plugin is stopping.")
-
-def onCommand(Unit, Command, Level, Hue):
-    global _pico
-
-    if (Command.upper() == 'ON'):
-        status = _pico.ledOn(deviceDict[Unit]["picoName"])
-        nValue = 1
-        sValue = "0"
-    else:
-        status = _pico.ledOff(deviceDict[Unit]["picoName"])
-        nValue = 0
-        sValue = "0"
-    
-    if status:
-        UpdateDevice(Unit, nValue, sValue)
-
-def onHeartbeat():
-    global _pico
-
-    result = _pico.getData()
-
-    if result:
-        UpdateDevice(1, 0, str(_pico.picoData["voltBat"]), updEvery=True)
-        UpdateDevice(2, 0, str(_pico.picoData["voltRpi"]), updEvery=True)
-        UpdateDevice(3, 0, str(_pico.picoData["tempNtc1"]), updEvery=True)
-        UpdateDevice(4, 0, _pico.picoData["pwrMode"])
-        UpdateDevice(5, _pico.picoData["ledOrange"], "0")
-        UpdateDevice(6, _pico.picoData["ledGreen"], "0")
-        UpdateDevice(7, _pico.picoData["ledBlue"], "0")
-        UpdateDevice(8, _pico.picoData["ledEnable"], "0")
-
-def UpdateDevice(Unit, nValue, sValue, updEvery=False):
-    # Make sure that the Domoticz device still exists (they can be deleted) before updating it
-    if (Unit in Devices):
-        if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue):
-            Devices[Unit].Update(nValue, str(sValue))
-            #Domoticz.Debug("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
-        elif updEvery:
-            # Update device even not changed every X minutes - for Temp and Volt values
-            updMinutes = 15
-            Domoticz.Debug("LastUpdate: '" + Devices[Unit].LastUpdate + "' ("+Devices[Unit].Name+")")
-            #devUpdate = datetime.strptime("2017-06-22 18:24:21", "%Y-%m-%d %H:%M:%S")
-            #devUpdate = datetime.strptime(Devices[Unit].LastUpdate, "%Y-%m-%d %H:%M:%S")
-            devUpdate = datetime(*(time.strptime(Devices[Unit].LastUpdate, "%Y-%m-%d %H:%M:%S")[0:6]))
-            devBefore = datetime.now() - devUpdate
-            if (devBefore > timedelta(minutes=updMinutes)):
-                Domoticz.Debug("Updated before: " + str(devBefore) + " ("+Devices[Unit].Name+")")
-                Devices[Unit].Update(nValue, str(sValue))
-    return
-
-# Generic helper functions
-def DumpConfigToLog():
-    for x in Parameters:
-        if Parameters[x] != "":
-            Domoticz.Debug( "'" + x + "':'" + str(Parameters[x]) + "'")
-    Domoticz.Debug("Device count: " + str(len(Devices)))
-    for x in Devices:
-        Domoticz.Debug("Device:           " + str(x) + " - " + str(Devices[x]))
-        Domoticz.Debug("Device ID:       '" + str(Devices[x].ID) + "'")
-        Domoticz.Debug("Device Name:     '" + Devices[x].Name + "'")
-        Domoticz.Debug("Device nValue:    " + str(Devices[x].nValue))
-        Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
-        Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
-    return
-
-def main():
-    _pico.getTest()
-
-if __name__ == '__main__':
-    main()
-"""
